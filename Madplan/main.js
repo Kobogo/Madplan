3c1d1f01e652a1f8b2c76c4216cd702b41d24c12
Adam Bang <39533056+Kobogo@users.noreply.github.com>
2022-08-29 13:06:19 +0200

    Update with Modal

--- Madplan/main.js
+++ Madplan/main.js
@@ -9,38 +9,38 @@
   const indkøb = document.querySelector('.indkøb')
   let btn = document.getElementById('#btn')
 
+  btn = document.addEventListener('click', generateMenu) 
+
   let mad = [
     {
       ret: "Hot dogs", 
-      ingradienser: ["Pølser", "Brød"]
+      ingredienser: ["Pølser", "Brød"]
     },
     {
-      ret: "Spareribs",
-      ingradienser: ["Spareribs", "Agurk", "Rød Peber", "Mejs", "Pasta", "Flutes"] 
+      ret: "Spareribs m. pastasalat",
+      ingredienser: ["Spareribs", "Agurk", "Rød Peber", "Mejs", "Pasta", "Flutes"] 
     },
     {
       ret: "Frisk pasta m. mornaysovs",
-      ingradienser: ["Frisk pasta", "Mornaysovs", "Bacon"]
+      ingredienser: ["Frisk pasta", "Mornaysovs", "Bacon"]
     },
     {
       ret: "Frikadeller m. ovnkartofler", 
-      ingradienser: ["HK kalv og flæsk", "Ovnkartofler"]
+      ingredienser: ["HK kalv og flæsk", "Ovnkartofler"]
     },
     {
       ret: "Tex mex", 
-      ingradienser: ["Pandekager", "krøderiger", "Oliven", "Revet ost", "relapinjos", "Majs", "Agurk", "Rød Peber"]
+      ingredienser: ["Pandekager", "krøderiger", "Oliven", "Revet ost", "relapinjos", "Majs", "Agurk", "Rød Peber"]
     },
     {
-      ret: "Stægt flæsk med parsillesovs", 
-      ingradienser: ["Flæsk", "Parsille", "Mælk", "Mel"]
+      ret: "Stægt flæsk med persillesovs", 
+      ingredienser: ["Flæsk", "Parsille", "Mælk", "Mel"]
     },
     {
       ret: "Indbagt laks m. flødekartofler", 
-      ingradienser: ["Indbagt laks", "Skivkartofler", "Fløde", "Løg"]
+      ingredienser: ["Indbagt laks", "Skivkartofler", "Fløde", "Løg"]
     }
   ];
-
-btn = document.addEventListener('click', generateMenu) 
 
 
 function setRandom(){
@@ -52,11 +52,19 @@ function getRandom(){
  
   let setNewRandom = [];
   
-/*    for (let index = 0; index < 1000; index++) {
-    setNewRandom[index] += setNewRandom.push(setRandom())
-    setNewRandom = [...new Set(setNewRandom)]
-  }  */
-   setNewRandom.push(setRandom())
+  /* for (let index = 0; index < 1000; index++) {
+    let random = setNewRandom[index] += setNewRandom.push(setRandom())
+    random = [...new Set(setNewRandom)]
+
+    mandag.textContent = " " + random[0].ret
+    tirsdag.textContent = " " + random[1].ret
+    onsdag.textContent = " " + random[2].ret
+    torsdag.textContent = " " + random[3].ret
+    fredag.textContent = " " + random[4].ret
+    lørdag.textContent = " " + random[5].ret
+    søndag.textContent = " " + random[6].ret
+
+  }  */ 
   setNewRandom.push(setRandom())
   setNewRandom.push(setRandom())
   setNewRandom.push(setRandom())
@@ -93,17 +101,18 @@ function getRandom(){
   setNewRandom.push(setRandom())
   setNewRandom.push(setRandom())
   setNewRandom.push(setRandom())
-  setNewRandom.push(setRandom())  
+  setNewRandom.push(setRandom())
+  setNewRandom.push(setRandom())   
   setNewRandom = [...new Set(setNewRandom)]
   
-
-    mandag.textContent = " " + setNewRandom[0].ret
-    tirsdag.textContent = " " + setNewRandom[1].ret
-    onsdag.textContent = " " + setNewRandom[2].ret
-    torsdag.textContent = " " + setNewRandom[3].ret
-    fredag.textContent = " " + setNewRandom[4].ret
-    lørdag.textContent = " " + setNewRandom[5].ret
-    søndag.textContent = " " + setNewRandom[6].ret
+  mandag.textContent = " " + setNewRandom[0].ret
+  tirsdag.textContent = " " + setNewRandom[1].ret
+  onsdag.textContent = " " + setNewRandom[2].ret
+  torsdag.textContent = " " + setNewRandom[3].ret
+  fredag.textContent = " " + setNewRandom[4].ret
+  lørdag.textContent = " " + setNewRandom[5].ret
+  søndag.textContent = " " + setNewRandom[6].ret 
+    
 
   /*   indkøb.textContent = " " + setNewRandom[0].ingradienser[0]
     indkøb.textContent = " " + setNewRandom[1].ingradienser[1]
@@ -120,8 +129,49 @@ function generateMenu(){
   getRandom()
 }
 
- 
+///////////////////////////// Modal open/close logic ///////////////////////////////////////////////////
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.querySelector('.overlay');
 
+openModalButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+overlay.addEventListener('click',() => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+ 
+/////////Create Ret //////////////
+
+const NyRet = [];
+
+NyRet.push(mad.ret)
 
 
 
